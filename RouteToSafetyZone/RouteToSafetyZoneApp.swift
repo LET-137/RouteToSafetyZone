a4aa5c71d0ca11dcb2c11f2460f2f27857d93754
LET_137 <bjs.6300.137@gmail.com>
2024-08-21 19:18:46 +0900

    Initial Commit

---
+++ RouteToSafetyZone/RouteToSafetyZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteToSafetyZoneApp.swift
+//  RouteToSafetyZone
+//
+//  Created by 津本拓也 on 2024/08/21.
+//
+
+import SwiftUI
+
+@main
+struct RouteToSafetyZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
